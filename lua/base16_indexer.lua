46f8c9a2a56eb20af5aa4e33164fdf13b951ee47
tami5 <kkharji@protonmail.com>
2022-02-05 23:29:52 +0300

    chore: remove generated files

--- lua/base16_indexer.lua
+++ lua/base16_indexer.lua
@@ -1,32 +1,3 @@
--- local cterm = {
---   ["00"] = "00",
---   ["03"] = "08",
---   ["05"] = "07",
---   ["07"] = "15",
---   ["08"] = "01",
---   ["0A"] = "03",
---   ["0B"] = "02",
---   ["0C"] = "06",
---   ["0D"] = "04",
---   ["0E"] = "05",
--- }
-
--- -- Terminal color definitions
--- if vim.g.base16_no_256_colorspace == nil then
---   cterm["01"] = "18"
---   cterm["02"] = "19"
---   cterm["04"] = "20"
---   cterm["06"] = "21"
---   cterm["09"] = "16"
---   cterm["0F"] = "17"
--- else
---   cterm["01"] = "10"
---   cterm["02"] = "11"
---   cterm["04"] = "12"
---   cterm["06"] = "13"
---   cterm["09"] = "09"
---   cterm["0F"] = "14"
--- end
 local cache
 local current_theme
 
